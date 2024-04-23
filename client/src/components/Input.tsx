a976906752c7195330ced7cb3e608f3c9e7c622d
Radostin Romanov <rad.romanov@gmail.com>
2024-04-23 12:03:54 +0300

    Add useSignup hook and custom Input component

---
+++ client/src/components/Input.tsx
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import { z } from "zod";
+
+interface Props<T>
+  extends React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  text: string;
+  type: React.HTMLInputTypeAttribute;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
+  disabled?: boolean;
+  required?: boolean;
+  autoFocus?: boolean;
+  schema: z.ZodSchema<T>;
+}
+
+function generateNameAndLabel(str: string) {
+  const splitStr = str.split(" ");
+  const strLen = splitStr.length;
+
+  let label = "";
+  let name = "";
+
+  if (strLen > 1) {
+    name = splitStr
+      .map((str, i) =>
+        i === 0
+          ? str.toLowerCase()
+          : str[0].toUpperCase() + str.slice(1, str.length)
+      )
+      .join("");
+    label = splitStr
+      .map((str) => str[0].toUpperCase() + str.slice(1, str.length))
+      .join(" ");
+  } else {
+    const str = splitStr.join();
+    name = str.toLowerCase();
+    label = str[0].toUpperCase() + str.slice(1, str.length);
+  }
+
+  return { name, label };
+}
+
+function Input<T>({
+  text,
+  type,
+  value,
+  onChange,
+  className,
+  disabled,
+  required = true,
+  autoFocus = false,
+  schema,
+  ...props
+}: Props<T>) {
+  const { name, label } = generateNameAndLabel(text);
+  const [error, setError] = useState<string | null>(null);
+
+  function validate(e: React.FocusEvent<HTMLInputElement, Element>) {
+    e.preventDefault();
+
+    const input = schema.safeParse(e.target.value);
+    if (!input.success) {
+      setError(input.error.issues[0].message);
+    } else {
+      setError(null);
+    }
+  }
+
+  return (
+    <label className="flex flex-col" htmlFor={name}>
+      <p>
+        <span className="font-semibold">{label}</span>
+      </p>
+      <input
+        className={`w-full border rounded-md px-3 py-1 ${className ?? ""} ${
+          disabled ? "cursor-not-allowed hover:bg-neutral-800" : ""
+        } ${error ? "border-red-500" : "border-neutral-700"}`}
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+        required={required}
+        autoFocus={autoFocus}
+        onBlur={validate}
+        {...props}
+      />
+      {error ? (
+        <p className="flex justify-center">
+          <span className="text-red-400">{error}</span>
+        </p>
+      ) : (
+        <></>
+      )}
+    </label>
+  );
+}
+
+export default Input;
